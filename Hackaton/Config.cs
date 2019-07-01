56ddf6b143b948f1693b073a31d2d03118f22f8e
camalzade_elvin <camalzade_elvin@outlook.com>
2019-07-01 20:07:19 +0400

    Add project files.

---
+++ Hackaton/Config.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Hackaton
+{
+    class Config
+    {
+        public string FileName { get; set; }
+        public Data Data { get; set; }
+
+        public void SeriailizeWordsToJson()
+        {
+            using (StreamWriter sw = new StreamWriter(FileName))
+            {
+                var item = JsonConvert.SerializeObject(Data);
+                sw.WriteLine(item);
+            }
+        }
+        public Data DeserializeWordsFromJson()
+        {
+            try
+            {
+                var context = File.ReadAllText(FileName);
+                Data = JsonConvert.DeserializeObject<Data>(context);
+            }
+            catch (Exception)
+            {
+            }
+            return Data;
+        }
+    }
+}
---
+++ Hackaton/Program.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+namespace Hackaton
+{
+    class Program
+    {
+        static string Hash(string input)
+        {
+            using (SHA1Managed sha1 = new SHA1Managed())
+            {
+                var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
+                var sb = new StringBuilder(hash.Length * 2);
+
+                foreach (byte b in hash)
+                {
+                    // can be "x2" if you want lowercase
+                    sb.Append(b.ToString("X2"));
+                }
+                return sb.ToString();
+            }
+        }
+        static async void Request(Data mydata)
+        {
+            string CateJson = JsonConvert.SerializeObject(mydata);
+            HttpClient Client = new HttpClient();
+            string baseAddress = "http://complete.azincloud.az:8880/register";
+            string Link = $"{baseAddress}?={CateJson}";
+            var Request = new HttpRequestMessage(HttpMethod.Post, Link);
+            HttpResponseMessage Message = await Client.SendAsync(Request).ConfigureAwait(false);
+            try
+            {
+                Message.EnsureSuccessStatusCode();
+                Console.WriteLine((int)Message.StatusCode);
+            }
+            catch (Exception)
+            {
+            }
+        }
+        static void Main(string[] args)
+        {
+            Data data = new Data()
+            { };
+            var hash = Hash("camalzade_elvin@mail.ru");
+            data.Email = "camalzade_elvin@mail.ru";
+            data.Key = hash;
+            Config config = new Config();
+            config.FileName = "data.json";
+            config.Data = data;
+            Data mydata = new Data();
+            if (File.Exists(config.FileName))
+            {
+                mydata = config.DeserializeWordsFromJson();
+                Console.WriteLine(mydata.Email);
+                Console.WriteLine(mydata.Key);
+            }
+            else
+            {
+                config.SeriailizeWordsToJson();
+            }
+            Request(mydata);
+        }
+    }
+}
